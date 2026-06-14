c6aac2f661adf0e9c5acc6911864447f666d61a9
luisAristeguieta <luisjimenezgx@gmail.com>
2026-06-13 21:44:42 -0400

    feat(data): add mock data for video games store with 6 games

---
+++ src/data/videojuegos.js
@@ -0,0 +1,71 @@
+// src/data/videojuegos.js
+const data = [
+    {
+        id: 1,
+        titulo: "Roblox",
+        genero: "Sandbox",
+        plataforma: "PC, Móvil, Xbox",
+        lanzamiento: 2006,
+        precio: 0,
+        disponible: true,
+        progreso: 0.75,
+        imagen: "https://photos5.appleinsider.com/gallery/40883-79111-B3840B94-33C9-4304-96D9-2BEBF83ECBAD-xl.jpg"
+    },
+    {
+        id: 2,
+        titulo: "Free Fire",
+        genero: "Battle Royale",
+        plataforma: "Móvil, PC",
+        lanzamiento: 2017,
+        precio: 0,
+        disponible: true,
+        progreso: 0.90,
+        imagen: "https://www.hd-tecnologia.com/imagenes/articulos/2020/05/El-battle-royale-Free-Fire-supera-los-80-millones-de-jugadores-diarios.jpg"
+    },
+    {
+        id: 3,
+        titulo: "League of Legends (LOL)",
+        genero: "MOBA",
+        plataforma: "PC",
+        lanzamiento: 2009,
+        precio: 0,
+        disponible: true,
+        progreso: 0.60,
+        imagen: "https://lwjtnezymbedzeqnggnb.supabase.co/storage/v1/object/public/blog-images/blog/1777628348065-0py3m.jpg"
+    },
+    {
+        id: 4,
+        titulo: "Fortnite",
+        genero: "Battle Royale",
+        plataforma: "PC, Móvil, Consolas",
+        lanzamiento: 2017,
+        precio: 0,
+        disponible: true,
+        progreso: 0.50,
+        imagen: "https://m.media-amazon.com/images/M/MV5BMTZlMmIxM2EtN2Y4Zi00M2ZhLTk3NzgtNjJmZTU0MTQ3YjcwXkEyXkFqcGc@._V1_.jpg"
+    },
+    {
+        id: 5,
+        titulo: "Minecraft",
+        genero: "Sandbox",
+        plataforma: "PC, Móvil, Consolas",
+        lanzamiento: 2011,
+        precio: 29.99,
+        disponible: true,
+        progreso: 0.95,
+        imagen: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/store/software/switch/70010000000964/a28a81253e919298beab2295e39a56b7a5140ef15abdb56135655e5c221b2a3a"
+    },
+    {
+        id: 6,
+        titulo: "Valorant",
+        genero: "FPS",
+        plataforma: "PC",
+        lanzamiento: 2020,
+        precio: 0,
+        disponible: true,
+        progreso: 0.40,
+        imagen: "https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/valo2.png"
+    }
+];
+
+export default data;
