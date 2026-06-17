5e77bdcd417176a3e4e2ac88b465f064c209e000
luisAristeguieta <luisjimenezgx@gmail.com>
2026-06-17 19:50:05 -0400

    chore: add react-router-dom dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import TablaVideojuegos from './components/TablaVideojuegos';
 import data from './data/videojuegos';
-import './App.css'
 
 function App() {
   // Estado para los videojuegos (usando los datos importados)
